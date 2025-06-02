1e2a23dbdc4448ad873c54f7236a943a9ae625ea
Ronin <bhabyaprakash01@gmail.com>
2025-06-03 01:48:13 +0530

    Update getuserinfo.py

--- Werewolf/plugins/base/getuserinfo.py
+++ Werewolf/plugins/base/getuserinfo.py
@@ -5,30 +5,36 @@ from Werewolf import app
 from Werewolf.plugins.base.db import group_log_db, global_userinfo_db
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from pyrogram.errors import PeerIdInvalid
 
 async def background_sync_loop():
     while True:
         group_names = await group_log_db.distinct("group_name")
-        
+
         for group_name in group_names:
             group_data = await group_log_db.find({"group_name": group_name}).to_list(None)
             if not group_data:
                 continue
 
             for data in group_data:
-                user_doc = {
-                    "user_id": data.get("user_id"),
-                    "username": data.get("username"),
-                    "group_name": group_name,
-                    "join_date": data.get("join_date", datetime.utcnow()),
-                    "activity": data.get("activity", "")
-                }
+                user_id = data.get("user_id")
+                if not user_id:
+                    continue
 
-                await global_userinfo_db.update_one(
-                    {"user_id": user_doc["user_id"], "group_name": group_name},
-                    {"$set": user_doc},
-                    upsert=True
-                )
+                try:
+                    user = await app.get_users(user_id)
+                    userinfo = {
+                        "_id": user.id,
+                        "first_name": user.first_name,
+                        "last_name": user.last_name,
+                        "username": user.username,
+                        "is_bot": user.is_bot
+                    }
+                    await global_userinfo_db.update_one({"_id": user.id}, {"$set": userinfo}, upsert=True)
+                except PeerIdInvalid:
+                    continue
+
+                await asyncio.sleep(random.uniform(0.5, 1.5))
 
             await asyncio.sleep(random.randint(2, 6))
 
@@ -54,28 +60,28 @@ async def sync_callback(client, callback_query: CallbackQuery):
         return
 
     group_name = callback_query.message.chat.title or str(callback_query.message.chat.id)
-    
+
     if callback_query.data == "sync_yes":
         group_data = await group_log_db.find({"group_name": group_name}).to_list(None)
-
         success = 0
+
         for data in group_data:
-            user_doc = {
-                "user_id": data.get("user_id"),
-                "username": data.get("username"),
-                "group_name": group_name,
-                "join_date": data.get("join_date", datetime.utcnow()),
-                "activity": data.get("activity", "")
-            }
+            user_id = data.get("user_id")
+            if not user_id:
+                continue
 
             try:
-                await global_userinfo_db.update_one(
-                    {"user_id": user_doc["user_id"], "group_name": group_name},
-                    {"$set": user_doc},
-                    upsert=True
-                )
+                user = await app.get_users(user_id)
+                userinfo = {
+                    "_id": user.id,
+                    "first_name": user.first_name,
+                    "last_name": user.last_name,
+                    "username": user.username,
+                    "is_bot": user.is_bot
+                }
+                await global_userinfo_db.update_one({"_id": user.id}, {"$set": userinfo}, upsert=True)
                 success += 1
-            except Exception:
+            except PeerIdInvalid:
                 continue
 
         await callback_query.message.reply_text(

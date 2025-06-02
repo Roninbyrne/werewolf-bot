e55c8833092474423fd753e5f83eb288fb3bb664
Ronin <bhabyaprakash01@gmail.com>
2025-06-02 22:17:38 +0530

    Create db.py

---
+++ Werewolf/plugins/base/db.py
@@ -0,0 +1,8 @@
+from pymongo import MongoClient
+from config import MONGO_DB_URI
+
+mongo_client = MongoClient(MONGO_DB_URI)
+db = mongo_client["storage"]
+
+group_log_db = db["group_logs"]
+global_ban_db = db["global_bans"]

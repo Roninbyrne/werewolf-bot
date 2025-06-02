db475e437110f34f009ff0ed88adc03dcb102f82
Ronin <bhabyaprakash01@gmail.com>
2025-06-02 13:58:12 +0530

    Create game.py

---
+++ Werewolf/plugins/werewolf/game.py
@@ -0,0 +1,9 @@
+from Werewolf import app
+from Werewolf.plugins.werewolf import (
+    werewolf_callbacks,
+    werewolf_commands,
+    werewolf_filters
+)
+
+if __name__ == "__main__":
+    app.run()

147d161a4dae42f64f9c085870285ead0d50a9c9
Ronin <bhabyaprakash01@gmail.com>
2025-06-02 12:49:10 +0530

    Update __main__.py

--- Werewolf/__main__.py
+++ Werewolf/__main__.py
@@ -5,23 +5,11 @@ from pyrogram import idle
 
 import config
 from Werewolf import LOGGER, app
-from Werewolf.misc import sudo
 from Werewolf.plugins import ALL_MODULES
-from Werewolf.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
 
 
 async def init():
-    await sudo()
-    try:
-        users = await get_gbanned()
-        for user_id in users:
-            BANNED_USERS.add(user_id)
-        users = await get_banned_users()
-        for user_id in users:
-            BANNED_USERS.add(user_id)
-    except:
-        pass
     await app.start()
     for all_module in ALL_MODULES:
         importlib.import_module("Werewolf.plugins" + all_module)

ab824a15a19d8ec6dbcb413ad18b794aea1ec8a5
Ronin <bhabyaprakash01@gmail.com>
2025-06-03 00:41:59 +0530

    Update db.py

--- Werewolf/plugins/base/db.py
+++ Werewolf/plugins/base/db.py
@@ -1,7 +1,7 @@
-from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
 from config import MONGO_DB_URI
 
-mongo_client = MongoClient(MONGO_DB_URI)
+mongo_client = AsyncIOMotorClient(MONGO_DB_URI)
 db = mongo_client["storage"]
 
 group_log_db = db["group_logs"]

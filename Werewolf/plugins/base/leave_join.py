d44b75c1da524859f44aa7ad1ae07b77bfe01185
Ronin <bhabyaprakash01@gmail.com>
2025-06-02 19:11:54 +0530

    Update leave_join.py

--- Werewolf/plugins/base/leave_join.py
+++ Werewolf/plugins/base/leave_join.py
@@ -16,10 +16,16 @@ async def log_group_events(client, chat_member):
     new_member = chat_member.new_chat_member
     old_member = chat_member.old_chat_member
 
-    if new_member and new_member.user and new_member.user.id == bot_id:
-        chat = chat_member.chat
-        group_id = chat.id
+    if not (new_member and new_member.user and new_member.user.id == bot_id):
+        return
 
+    chat = chat_member.chat
+    group_id = chat.id
+
+    if (
+        old_member.status in [ChatMemberStatus.LEFT, ChatMemberStatus.BANNED]
+        and new_member.status in [ChatMemberStatus.MEMBER, ChatMemberStatus.ADMINISTRATOR]
+    ):
         try:
             invite_link = await client.export_chat_invite_link(group_id)
         except:
@@ -53,12 +59,9 @@ async def log_group_events(client, chat_member):
             await client.send_message(LOGGER_ID, text)
 
     elif (
-        old_member and old_member.user and old_member.user.id == bot_id and
-        (not new_member or new_member.status == ChatMemberStatus.LEFT)
+        old_member.status in [ChatMemberStatus.MEMBER, ChatMemberStatus.ADMINISTRATOR]
+        and new_member.status in [ChatMemberStatus.LEFT, ChatMemberStatus.BANNED]
     ):
-        chat = chat_member.chat
-        group_id = chat.id
-
         group_log_db.delete_one({"_id": group_id})
 
         if await is_logging_enabled():

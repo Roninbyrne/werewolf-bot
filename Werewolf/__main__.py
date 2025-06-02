a994533aca6d3b5ba616a918910a3e23a7b7c632
Ronin <bhabyaprakash01@gmail.com>
2025-06-02 12:42:22 +0530

    Create __main__.py

---
+++ Werewolf/__main__.py
@@ -0,0 +1,39 @@
+import asyncio
+import importlib
+
+from pyrogram import idle
+
+import config
+from Security import LOGGER, app
+from Security.misc import sudo
+from Security.plugins import ALL_MODULES
+from Security.utils.database import get_banned_users, get_gbanned
+from config import BANNED_USERS
+
+
+async def init():
+    await sudo()
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("Security.plugins" + all_module)
+    LOGGER("Security.plugins").info("Successfully Imported Modules...")
+    LOGGER("Security").info(
+        "\x41\x6e\x6f\x6e\x58\x20\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\n\n\x44\x6f\x6e'\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x46\x61\x6c\x6c\x65\x6e\x41\x73\x73\x6f\x63\x69\x61\x74\x69\x6f\x6e"
+    )
+    await idle()
+    await app.stop()
+    await userbot.stop()
+    LOGGER("Security").info("Stopping Security Bot...")
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(init())

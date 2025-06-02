f613283c9f803e6cfeaeac8f4916cea041c98f82
Ronin <bhabyaprakash01@gmail.com>
2025-06-02 14:00:52 +0530

    Update game.py

--- Werewolf/plugins/werewolf/game.py
+++ Werewolf/plugins/werewolf/game.py
@@ -1,8 +1,8 @@
 from Werewolf import app
 from Werewolf.plugins.werewolf import (
-    werewolf_callbacks,
-    werewolf_commands,
-    werewolf_filters
+    callbacks,
+    commands,
+    filters
 )
 
 if __name__ == "__main__":

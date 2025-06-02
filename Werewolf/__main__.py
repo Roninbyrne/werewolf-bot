1e11f1b7a850e9fb408111d75a14ed6bb0a2f780
Ronin <bhabyaprakash01@gmail.com>
2025-06-03 02:50:11 +0530

    Update __main__.py

--- Werewolf/__main__.py
+++ Werewolf/__main__.py
@@ -5,10 +5,12 @@ from pyrogram import idle
 from Werewolf import LOGGER, app
 from Werewolf.plugins import ALL_MODULES
 from Werewolf.plugins.base.leave_join import start_removal_monitor
+from Werewolf.plugins.base.userinfo import setup_user_tracking
 
 
 async def init():
     await app.start()
+    setup_user_tracking(app)
     start_removal_monitor()
     for all_module in ALL_MODULES:
         importlib.import_module("Werewolf.plugins" + all_module)

6df101d90329c0a4da903483aed0678578eba048
Ronin <bhabyaprakash01@gmail.com>
2025-06-03 16:48:56 +0530

    Update __init__.py

--- Werewolf/__init__.py
+++ Werewolf/__init__.py
@@ -1,10 +1,11 @@
-from Werewolf.core.bot import Anony
 from Werewolf.core.dir import dirr
 from Werewolf.core.git import git
+from Werewolf.core.bot import app
+from Werewolf.core.bot import start_bot
 
 from .logging import LOGGER
 
 dirr()
 git()
 
-app = Anony()
+__all__ = ["app", "start_bot"]

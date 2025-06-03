839e963c32e447fdc856c1871e05b913faa8dcae
Ronin <bhabyaprakash01@gmail.com>
2025-06-03 17:34:43 +0530

    Update bot.py

--- Werewolf/core/bot.py
+++ Werewolf/core/bot.py
@@ -5,7 +5,7 @@ import config
 from ..logging import LOGGER
 
 app = Client(
-    name="Security",
+    name="Werewolf",
     api_id=config.API_ID,
     api_hash=config.API_HASH,
     bot_token=config.BOT_TOKEN,

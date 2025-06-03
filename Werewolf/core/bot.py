c880926cf8d3c582707ee051fe7b5e258bae1f2f
Ronin <bhabyaprakash01@gmail.com>
2025-06-03 16:48:15 +0530

    Update bot.py

--- Werewolf/core/bot.py
+++ Werewolf/core/bot.py
@@ -2,53 +2,47 @@ from pyrogram import Client, errors
 from pyrogram.enums import ChatMemberStatus, ParseMode
 
 import config
-
 from ..logging import LOGGER
 
+app = Client(
+    name="Security",
+    api_id=config.API_ID,
+    api_hash=config.API_HASH,
+    bot_token=config.BOT_TOKEN,
+    in_memory=True,
+    parse_mode=ParseMode.HTML,
+    max_concurrent_transmissions=7,
+)
 
-class Anony(Client):
-    def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot...")
-        super().__init__(
-            name="Security",
-            api_id=config.API_ID,
-            api_hash=config.API_HASH,
-            bot_token=config.BOT_TOKEN,
-            in_memory=True,
-            parse_mode=ParseMode.HTML,
-            max_concurrent_transmissions=7,
+async def start_bot():
+    await app.start()
+    me = await app.get_me()
+    app.id = me.id
+    app.name = me.first_name + " " + (me.last_name or "")
+    app.username = me.username
+    app.mention = me.mention
+
+    try:
+        await app.send_message(
+            chat_id=config.LOGGER_ID,
+            text=f"<u><b>» {app.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b></u>\n\nɪᴅ : <code>{app.id}</code>\nɴᴀᴍᴇ : {app.name}\nᴜsᴇʀɴᴀᴍᴇ : @{app.username}",
         )
+    except (errors.ChannelInvalid, errors.PeerIdInvalid):
+        LOGGER(__name__).error(
+            "Bot has failed to access the log group/channel. Make sure that you have added your bot to your log group/channel."
+        )
+        exit()
+    except Exception as ex:
+        LOGGER(__name__).error(
+            f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).__name__}."
+        )
+        exit()
 
-    async def start(self):
-        await super().start()
-        self.id = self.me.id
-        self.name = self.me.first_name + " " + (self.me.last_name or "")
-        self.username = self.me.username
-        self.mention = self.me.mention
+    status = await app.get_chat_member(config.LOGGER_ID, app.id)
+    if status.status != ChatMemberStatus.ADMINISTRATOR:
+        LOGGER(__name__).error(
+            "Please promote your bot as an admin in your log group/channel."
+        )
+        exit()
 
-        try:
-            await self.send_message(
-                chat_id=config.LOGGER_ID,
-                text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
-            )
-        except (errors.ChannelInvalid, errors.PeerIdInvalid):
-            LOGGER(__name__).error(
-                "Bot has failed to access the log group/channel. Make sure that you have added your bot to your log group/channel."
-            )
-            exit()
-        except Exception as ex:
-            LOGGER(__name__).error(
-                f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).__name__}."
-            )
-            exit()
-
-        a = await self.get_chat_member(config.LOGGER_ID, self.id)
-        if a.status != ChatMemberStatus.ADMINISTRATOR:
-            LOGGER(__name__).error(
-                "Please promote your bot as an admin in your log group/channel."
-            )
-            exit()
-        LOGGER(__name__).info(f"Bot Started as {self.name}")
-
-    async def stop(self):
-        await super().stop()
+    LOGGER(__name__).info(f"Bot Started as {app.name}")

a89ab3e5f37f33bab15118d4df5bb81ceab4bf07
Ronin <bhabyaprakash01@gmail.com>
2025-06-03 16:49:11 +0530

    Update __main__.py

--- Werewolf/__main__.py
+++ Werewolf/__main__.py
@@ -2,11 +2,11 @@ import asyncio
 import importlib
 from pyrogram import idle
 
-from Werewolf import LOGGER, app
+from Werewolf import LOGGER, app, start_bot
 from Werewolf.plugins import ALL_MODULES
 
 async def init():
-    await app.start()
+    await start_bot()
     for all_module in ALL_MODULES:
         importlib.import_module("Werewolf.plugins" + all_module)
     LOGGER("Werewolf.plugins").info("Successfully Imported Modules...")
@@ -14,7 +14,6 @@ async def init():
     await idle()
     await app.stop()
     LOGGER("Werewolf").info("Stopping Werewolf Game Bot...")
-
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(init())

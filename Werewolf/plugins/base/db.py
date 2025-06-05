bd625933128b32c99b08c84122cf9351f56e23ac
Ronin <bhabyaprakash01@gmail.com>
2025-06-05 12:55:23 +0530

    Update db.py

--- Werewolf/plugins/base/db.py
+++
@@ -1,8 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from config import MONGO_DB_URI
-
-mongo_client = AsyncIOMotorClient(MONGO_DB_URI)
-db = mongo_client["storage"]
-
-global_ban_db = db["global_bans"]
-global_userinfo_db = db["user_info"]

81d69d0c528653a7260d5613e34c7d7640031b34
Ronin <bhabyaprakash01@gmail.com>
2025-06-22 12:23:09 +0530

    Update __init__.py

--- Werewolf/__init__.py
+++ Werewolf/__init__.py
@@ -1,11 +1,9 @@
 from Werewolf.core.dir import dirr
-from Werewolf.core.git import git
 from Werewolf.core.bot import app
 from Werewolf.core.bot import start_bot
 
 from .logging import LOGGER
 
 dirr()
-git()
 
 __all__ = ["app", "start_bot"]

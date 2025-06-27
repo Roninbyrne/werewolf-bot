70f1a6f50d1d17cd5ee158c5d535e9fddd15a872
Ronin <bhabyaprakash01@gmail.com>
2025-06-27 07:09:11 +0530

    Delete Werewolf/plugins/werewolf directory

--- Werewolf/plugins/werewolf/db.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-from config import MONGO_DB_URI
-
-mongo_client = MongoClient(MONGO_DB_URI)
-db = mongo_client["werewolf_bot"]
-games_col = db.games
-players_col = db.players
-actions_col = db.actions
--- Werewolf/plugins/werewolf/game.py
+++
@@ -1,9 +0,0 @@
-from Werewolf import app
-from Werewolf.plugins.werewolf import (
-    callbacks,
-    commands,
-    filters
-)
-
-if __name__ == "__main__":
-    app.run()
